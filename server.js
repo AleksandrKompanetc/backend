5edb613d720ede1ba3ce9a4426bbb6b6a91de31e
AleksandrKompanetc <moneymacker7@gmail.com>
2025-10-23 21:28:14 +0300

    added switch

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const http = require('http')
 
+let requestCount = 0
+
 const server = http.createServer((req, res) => {
-  res.write('Server work')
+  requestCount++
+
+  switch(req.url) {
+    case '/students':
+      res.write('STUDENTS')
+      break
+    case '/courses':
+      res.write('FRONT + BACK')
+      break
+    default:
+      res.write('404 Not found')
+  }
+
+  res.write('Server work'+ requestCount)
   res.end()
 })
 

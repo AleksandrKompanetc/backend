f02e2b46f5fa11965e5e7c3015c7a7d8a94e7ff0
AleksandrKompanetc <moneymacker7@gmail.com>
2025-10-24 23:56:32 +0300

    added try catch

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const delay = (ms) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve()
-  }, ms)
+    }, ms)
   })
 }
 
@@ -15,22 +15,26 @@ const readFile = (path) => {
   return new Promise((resolve, reject) => {
     fs.readFile(path, (err, data) => {
       if (err) reject(err)
-        else resolve(data)
+      else resolve(data)
     })
   })
 }
 
-const server = http.createServer((request, response) => {
+const server = http.createServer(async (request, response) => {
   requestCount++
 
   switch (request.url) {
-    case '/home':
-      fs.readFile('pages/home.html', (error, data) => {
-        if (error) response.write(error)
-        else response.write(data)
+    case '/home': {
+      try {
+        const data = await readFile('pages/home.html')
+        response.write(data)
         response.end()
-      }) //'best free online'
+      } catch (err) {
+        response.write('something wrong, 500')
+        response.end()
+      }
       break
+    }
     case '/about': {
       await delay(3000)
       response.write('ABOUT')

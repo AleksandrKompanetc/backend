b64c6e760bd57454a46be8c9bbc5693b27cfedca
AleksandrKompanetc <moneymacker7@gmail.com>
2025-10-28 23:46:46 +0200

    added typescript for app

---
+++ index.js
@@ -0,0 +1,24 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const port = 3013;
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+app.get('/about', (req, res) => {
+    res.send('About Page');
+});
+app.get('/samur', (req, res) => {
+    res.send('Hello samurai, Sasha!');
+});
+app.get('/samurais', (req, res) => {
+    res.send('Hello samurai, Sasha!');
+});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+//# sourceMappingURL=index.js.map

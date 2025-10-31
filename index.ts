e9540fb336b8222439e366f4aa374dd56abfd9ff
AleksandrKompanetc <moneymacker7@gmail.com>
2025-10-31 23:27:34 +0200

    installed typescript

---
+++ index.ts
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const port = 3002;
+
+app.get('/', (req, res) => {
+  const a = 8;
+  if (a > 5) {
+    res.send('OK');
+  } else {
+    res.send('Not OK');
+  }
+  // res.send('Hello world!!!!!!!!');
+})
+
+app.get('/samurais', (req, res) => {
+  res.send('Samurais!!!!!!!!!!');
+})
+
+app.get('/about', (req, res) => {
+  res.send('About us page');
+})
+
+app.post('/samurais', (req, res) => {
+  res.send('We have created a new samurai!');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+})

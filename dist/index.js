9aabb438ead77b7f89f1341a7639f992347908b6
AleksandrKompanetc <moneymacker7@gmail.com>
2025-11-02 16:54:04 +0200

    added some changes

--- dist/index.js
+++ dist/index.js
@@ -10,12 +10,17 @@ app.get('/courses', (req, res) => {
     ]);
 });
 app.get('/courses/:id', (req, res) => {
-    res.json([
+    const foundCourse = [
         { id: 1, title: 'Frontend' },
         { id: 2, title: 'Backend' },
         { id: 3, title: 'Fullstack' },
         { id: 4, title: 'DevOps' }
-    ]).find(c => c.id === +req.params.id);
+    ].find(c => c.id === +req.params.id);
+    if (!foundCourse) {
+        res.sendStatus(404);
+        return;
+    }
+    res.json(foundCourse);
 });
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
--- src/index.ts
+++ src/index.ts
@@ -12,12 +12,18 @@ app.get('/courses', (req, res) => {
 })
 
 app.get('/courses/:id', (req, res) => {
-  res.json([
+  const foundCourse = [
     { id: 1, title: 'Frontend' },
     { id: 2, title: 'Backend' },
     { id: 3, title: 'Fullstack' },
     { id: 4, title: 'DevOps' }
-  ]).find(c => c.id === +req.params.id);
+  ].find(c => c.id === +req.params.id);
+
+  if (!foundCourse) {
+    res.sendStatus(404);
+    return;
+  }
+  res.json(foundCourse);
 })
 
 app.listen(port, () => {
